eafd1337a3557c5a0a6d2540bbd757231e8a21e5
Allen Kao <kaodondon7@gmail.com>
2021-12-15 15:32:15 +0800

    update wallet struct

--- structs/wallet.go
+++ structs/wallet.go
@@ -1,11 +1,19 @@
 package structs
 
+import "time"
+
 type Wallet struct {
-	Id      string `json:"id" bson:"id"`
-	UserId  string `json:"user_id" bson:"user_id"`
-	Balance []struct {
-		Amount   int      `json:"amount" bson:"amount"`
-		Unit     unit     `json:"unit" bson:"unit"` // should be the min unit
-		Currency currency `json:"currency" bson:"currency"`
-	} `json:"balance" bson:"balance"`
+	Id     string `gorm:"type:VARCHAR(36) NOT NULL primary_key;"`
+	UserId string `gorm:"type:VARCHAR(36) NOT NULL index;"`
+	//Status string
+	CreatedAt time.Time `gorm:"type:timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP"`
+	UpdatedAt time.Time `gorm:"type:timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"`
+}
+
+type Balance struct {
+	WalletId string   `gorm:"type:VARCHAR(36) NOT NULL index"`
+	Currency currency `gorm:"type:VARCHAR(36) NOT NULL"`
+	Amount   int      `gorm:"type:bigint(20);"`
+	Unit     unit     `gorm:"type:VARCHAR(10) NOT NULL"` // should be the min unit
+	UpdatedAt time.Time `gorm:"type:timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"`
 }
