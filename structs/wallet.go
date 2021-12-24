73540a199331dc1b07a957e09c9172ecd6ab3651
Allen Kao <kaodondon7@gmail.com>
2021-12-24 16:03:02 +0800

    update index

--- structs/wallet.go
+++ structs/wallet.go
@@ -2,7 +2,7 @@ package structs
 
 type Wallet struct {
 	Id     string `gorm:"primaryKey;column:id;type:VARCHAR(36) NOT NULL"`
-	UserId string `gorm:"index:user_id;column:userId;type:VARCHAR(36) NOT NULL"`
+	UserId string `gorm:"uniqueIndex:user_id;column:userId;type:VARCHAR(36) NOT NULL"`
 	//Status string
 	CreatedAt int64 `gorm:"column:createdAt;autoCreateTime"`
 	UpdatedAt int64 `gorm:"column:updatedAt;autoUpdateTime:milli"`
@@ -13,7 +13,7 @@ func (w Wallet) TableName() string {
 }
 
 type Balance struct {
-	WalletId string `gorm:"column:walletId;type:VARCHAR(36) NOT NULL;index:walletId_currency,unique"`
+	WalletId string `gorm:"column:walletId;type:VARCHAR(36) NOT NULL;uniqueIndex:walletId_currency,unique"`
 	Money
 	//Currency  Currency `gorm:"column:currency;type:VARCHAR(36) NOT NULL;index:walletId_currency,unique"`
 	//Amount    uint     `gorm:"column:amount;type:bigint(20);default:0"`
@@ -32,7 +32,7 @@ type WalletData struct {
 }
 
 type Money struct {
-	Currency Currency `gorm:"column:currency;type:VARCHAR(36) NOT NULL;index:walletId_currency,unique"`
+	Currency Currency `gorm:"column:currency;type:VARCHAR(36) NOT NULL;uniqueIndex:walletId_currency,unique"`
 	Amount   uint     `gorm:"column:amount;type:bigint(20);default:0"`
 	Unit     Unit     `gorm:"column:unit;type:VARCHAR(10) NOT NULL;default:pico"` // should be the min Unit
 }
